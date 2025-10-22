2197037834cbe5ad7a044f8548661a0cbe2b9b44
TwayigizeBienvenue <twayigize.niyitegeka@strathmore.edu>
2025-10-22 18:15:47 +0300

    remove duplicate

--- config.py
+++ config.py
@@ -5,9 +5,3 @@ DB_CONFIG = {
     "password": "",        # Your/My phpMyAdmin password
     "database": "lostfound_db"
 }
-DB_CONFIG = {
-    "host": "localhost",
-    "user": "root",
-    "password": "",        # Your/My phpMyAdmin password
-    "database": "lostfound_db"
-}
